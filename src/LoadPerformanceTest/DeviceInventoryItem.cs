98ebbcd17b294a5cf1f9e13670bfe59ccf5fc0d2
rsingh <ravi.singh@hach.com>
2026-04-20 12:24:51 +0530

    Remove DeviceInventoryItem class and its properties
    The `DeviceInventoryItem` class has been completely removed
    from the `LoadPerformanceTest` namespace, along with its
    properties: `InstrumentId`, `FusionId`, and `TenantId`.

--- src/LoadPerformanceTest/DeviceInventoryItem.cs
+++
@@ -1,8 +0,0 @@
-namespace LoadPerformanceTest;
-
-public class DeviceInventoryItem
-{
-    public string InstrumentId { get; set; }
-    public string FusionId { get; set; }
-    public string TenantId { get; set; }
-}
